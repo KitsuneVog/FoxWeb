38aef87b620108ea9d50758b1faf68688d84f3bb
KitsuneVog <galchenkovvladislav@gmail.com>
2023-01-11 20:25:16 +0300

    /

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+$(function() {
+
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH;
+  let scrollPos = $(window).scrollTop();
+
+  $(window).on("scroll load resize", function() {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    if(scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  })
+});
+
 function copyIP() {
     navigator.clipboard.writeText('play.vibecraft.ru')
     alert("ip-адрес скопирован");
