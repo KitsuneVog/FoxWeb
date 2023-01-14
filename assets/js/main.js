683d6fe60b201efb6efd9e7416ac37149fc1fef4
KitsuneVog <galchenkovvladislav@gmail.com>
2023-01-14 23:59:00 +0300

    /

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,30 @@
 $(function() {
 
-  let header = $("#header");
-  let intro = $("#intro");
-  let introH;
-  let scrollPos = $(window).scrollTop();
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH;
+	let scrollPos = $(window).scrollTop();
 
-  $(window).on("scroll load resize", function() {
-    introH = intro.innerHeight();
-    scrollPos = $(this).scrollTop();
+	$(window).on("scroll load resize", function() {
+    	introH = intro.innerHeight();
+    	scrollPos = $(this).scrollTop();
 
-    if(scrollPos > introH) {
-      header.addClass("fixed");
-    } else {
-      header.removeClass("fixed");
-    }
-  })
+    	if(scrollPos > introH) {
+    	header.addClass("fixed");
+    	} else {
+    	header.removeClass("fixed");
+    	}
+	})
 });
 
 function copyIP() {
-    navigator.clipboard.writeText('play.vibecraft.ru')
-    alert("ip-адрес скопирован");
+	navigator.clipboard.writeText('play.vibecraft.ru')
+
+	let div = document.createElement('div');
+
+    div.className = "alert";
+    div.innerHTML = "<b>ip-адрес скопирован!</b> Приятной игры.";
+	document.body.before(div);
+
+	setTimeout(() => div.remove(), 1500);
   }
