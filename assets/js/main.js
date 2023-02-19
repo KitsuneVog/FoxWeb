821aa286c4465dfafccb7fe15b9edc4d94701959
KitsuneVog <galchenkovvladislav@gmail.com>
2023-02-19 13:48:10 +0300

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ $(function() {
 	});
 
 	function checkScroll(scrollPos, introH) {
-		if(scrollPos > introH) {
+		if(scrollPos > introH - 50) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
